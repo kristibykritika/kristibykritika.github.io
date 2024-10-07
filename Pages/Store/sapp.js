fb3607f3e95cf84a4fa539342ba5516468442cb3
Soul Fire <shiladityabagchi1256@gmail.com>
2024-10-07 14:35:44 +0530

    Added a WP Store and contact section

--- Pages/Store/sapp.js
+++ Pages/Store/sapp.js
@@ -32,3 +32,7 @@ const dropDownMenu = document.querySelector('.dropdown_menu');
 toggleBtn.onclick = function () {
     dropDownMenu.classList.toggle('open');
 }
+
+
+
+
--- homeapp.js
+++ homeapp.js
@@ -5,3 +5,14 @@ const dropDownMenu = document.querySelector('.dropdown_menu');
 toggleBtn.onclick = function () {
     dropDownMenu.classList.toggle('open');
 }
+
+const cttButton = document.querySelector('.ctnBut');
+const strButton = document.querySelector('.strBut');
+
+cttButton.onclick = function() {
+window.open("https://wa.me/916297350047", "_blank");
+}
+
+strButton.onclick = function() {
+window.open("https://wa.me/c/916297350047", "_blank");
+}
